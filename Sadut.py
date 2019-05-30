de94da2d314918526cb5d04a4c6e06f3e755857b
CyberDomba <50538340+CyberDomba@users.noreply.github.com>
2019-05-30 21:31:50 +0700

    Update Sadut.py

--- Sadut.py
+++ Sadut.py
@@ -1,4 +1,4 @@
-figlet Cyber Domba | lolcat
+#figlet Cyber Domba | lolcat
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
