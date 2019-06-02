57d16c53446517edddbacedec33d6c6d66489b73
CyberDomba <50538340+CyberDomba@users.noreply.github.com>
2019-06-02 23:50:06 +0700

    Update Sadut.py

--- Sadut.py
+++ Sadut.py
@@ -177,7 +177,7 @@ def pilih():
                     else:
                         if zedd == '5':
                             os.system('rm -rf login.txt')
-                            os.system('xdg-open https://www.youtube.com/nganunymous')
+                            os.system('xdg-open https://www.youtube.com/channel/UCKdSySlEqFXVnR3-1TWD42Q')
                             keluar()
                         else:
                             if zedd == '0':
